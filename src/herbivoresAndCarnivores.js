34b380d318dffd6deeaeaa06ffb5630c3353cc49
milton <mitnunes.pro.com>
2025-09-02 16:03:43 -0300

    add 1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,54 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.healthy = 100;
+
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const idx = Animal.alive.indexOf(this);
+    if (idx !== -1) {
+      Animal.alive.splice(idx, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
-}
+  constructor(name) {
+    super(name);
+  }
+
+  bite(target) {
+    if (!(target instanceof Herbivore)) {
+      return;
+  }
+    if (target.hidden) {
+      return;
+   }
+
+   target.healthy -= 50;
+
+   if (target.healthy <= 0) {
+     target.die();
+      }
+    }
+  }
 
 module.exports = {
   Animal,
